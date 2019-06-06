3423b0031ca6974b151b78807ac81b30d7ab42d4
surajt <smartsurajit2008@gmail.com>
2019-06-06 18:10:53 -0400

    files

--- DjangoReact/settings.py
+++ DjangoReact/settings.py
@@ -103,13 +103,13 @@ DATABASES = {
         'NAME': 'djangoreact',
         'USER': 'test',
         'PASSWORD': 'abc123**!!',
-        'HOST': 'djangoreactcafe.herokuapp.com',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
