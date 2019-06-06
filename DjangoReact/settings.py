7858dbd618d30bb2ac5aba4dd98a25221eb6fa84
surajt <smartsurajit2008@gmail.com>
2019-06-06 18:20:24 -0400

    files

--- DjangoReact/settings.py
+++ DjangoReact/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,4 +151,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/upload/')
 MEDIA_URL = os.path.join(BASE_DIR, 'static/media/upload/')
 CORS_ORIGIN_ALLOW_ALL = True
+django_heroku.settings(locals())
 
