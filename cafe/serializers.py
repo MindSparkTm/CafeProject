2c602c15a68d2ae4f1d55a15b3cbeadd3064b50f
MindSparkTm <smartsurajit2008@gmail.com>
2019-06-07 08:28:07 -0400

    added type mapping to unicode string

--- cafe/serializers.py
+++ cafe/serializers.py
@@ -1,4 +1,3 @@
-from djmoney.money import Money
 from rest_framework import  serializers
 from .models import Menu
 
@@ -6,4 +5,21 @@ class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = '__all__'
+
+    def to_representation(self, obj):
+        data = super(MenuItemSerializer, self).to_representation(obj)
+        if 'type' in data:
+            if data['type']==1:
+                data['type'] = 'Side'
+            elif data['type']==2:
+                data['type'] = 'Main Course'
+            elif data['type'] ==3:
+                data['type'] = 'Appetizer'
+            elif data['type']==4:
+                data['type'] = 'Dessert'
+            else:
+                data['type'] = 'Not Specified'
+        return data
+
+
 
